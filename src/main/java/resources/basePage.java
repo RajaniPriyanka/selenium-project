e5873f1cef2877e455f1e0ff9a36aea92adbc264
RajaniPriyanka <rpchirus@gmail.com>
2020-06-21 23:32:02 -0700

    Update basePage.java

--- src/main/java/resources/basePage.java
+++ src/main/java/resources/basePage.java
@@ -27,7 +27,7 @@ public class basePage {
 	{
 		try {
 		   prop =new Properties();
-		   FileInputStream fis=new FileInputStream(System.getProperty("user.dir")+"\\data.properties");
+		   FileInputStream fis=new FileInputStream(System.getProperty("user.dir")+"/data.properties");
 		   prop.load(fis);
 		}catch(Exception e) {
 		   e.printStackTrace();

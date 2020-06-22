d3945a9398da53a851c8512cace8f1cfae292127
RajaniPriyanka <rpchirus@gmail.com>
2020-06-21 17:38:22 -0700

    Update homepage.java

--- src/main/java/zwiftProject/zwiftProjectSample/homepage.java
+++ src/main/java/zwiftProject/zwiftProjectSample/homepage.java
@@ -10,9 +10,6 @@ import resources.basePage;
 public class homepage extends basePage {
 
 public WebElement eventElement;
-	//@FindBy(css="a[href='/events']")
-	//WebElement event;
-	//By eventnavlink=By.linkText("Events");
 
 public WebElement eventsNavbar(WebDriver driver) {
 	eventElement =  driver.findElement(By.xpath("//div[contains(@class,'znv-d-none znv-d-lg-flex')]//a[contains(@class,'znv-d-inline-block')][contains(text(),'Events')]"));
